57fbf54f1243c67bb0a3f656a7344e4cf2b372b5
Donna Ryan <donna@nerdrose.com>
2016-10-18 17:12:37 -0700

    Initial status - accepts prompts for first and last name

---
+++ src/FullName/wwwroot/app.js
@@ -0,0 +1,4 @@
+var fname = prompt("What is your first name?");
+var lname = prompt("What is your last name?");
+alert('Welcome to my page, ' + fname + ' ' + lname);
+//# sourceMappingURL=app.js.map
---
+++ src/FullName/wwwroot/app.ts
@@ -0,0 +1,4 @@
+﻿var fname = prompt("What is your first name?");
+var lname = prompt("What is your last name?");
+
+alert('Welcome to my page, ' + fname + ' ' + lname);
