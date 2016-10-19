29bbad266df1305bad8550b7174404eb50435299
Donna Ryan <donna@nerdrose.com>
2016-10-18 17:30:38 -0700

    Adding the first name/last name prompts as input fields

--- src/FullName/wwwroot/app.js
+++ src/FullName/wwwroot/app.js
@@ -1,7 +1,9 @@
-var greetBtn = document.getElementById('greetMe');
+var greetBtn = document.getElementById("greetMe");
 greetBtn.addEventListener('click', function () {
-    var fname = prompt("What is your first name?");
-    var lname = prompt("What is your last name?");
-    alert('Welcome to my page, ' + fname + ' ' + lname);
+    // var fname = prompt("What is your first name?");
+    // var lname = prompt("What is your last name?");
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    alert("Welcome to my page, " + fname + " " + lname);
 });
 //# sourceMappingURL=app.js.map
--- src/FullName/wwwroot/app.ts
+++ src/FullName/wwwroot/app.ts
@@ -1,9 +1,10 @@
-﻿let greetBtn = document.getElementById('greetMe');
+﻿let greetBtn = (<HTMLButtonElement>document.getElementById("greetMe"));
 greetBtn.addEventListener('click', function () {
-    var fname = prompt("What is your first name?");
-    var lname = prompt("What is your last name?");
-
-    alert('Welcome to my page, ' + fname + ' ' + lname);
-})
+    // var fname = prompt("What is your first name?");
+    // var lname = prompt("What is your last name?");
+    var fname:string = (<HTMLInputElement>document.getElementById("fname")).value;
+    var lname:string = (<HTMLInputElement>document.getElementById("lname")).value;
+    alert("Welcome to my page, " + fname + " " + lname);
+});
 
 
